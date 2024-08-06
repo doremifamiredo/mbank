26885ce07df3bc412498f03517efa3843f705c38
Oleg Somov <doremi@fasollasi.ru>
2024-08-06 17:57:40 +0300

    Initial commit

---
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -0,0 +1,29 @@
+package ru.netology.rest;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+class MobileBankApiTestV1 {
+    @Test
+    void shouldReturnDemoAccounts() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .header("content-type","application/json; charset=UTF-8")
+                .contentType(ContentType.JSON)
+                .body("", hasSize(3))
+                .body("[0].id", equalTo(1))
+                .body("[0].currency", equalTo("RUB"))
+                .body("[1].balance", greaterThan(0));
+    }
+}
